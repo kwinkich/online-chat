6f7592b93948dccfb60ee6dc6bfe784514c87eaa
kwinkich <vankord21@gmail.com>
2023-02-23 12:45:03 +0300

    add new file

--- main.py
+++ main.py
@@ -92,10 +92,12 @@
 
 import asyncio
 
+
 from pywebio import start_server
 from pywebio.input import *
 from pywebio.output import *
 from pywebio.session import defer_call, info as session_info, run_async, run_js
+
 
 chat_msgs = []
 online_users = set()
